2215aae945e732318dc377f12b16eb0cd291e625
Arseniy Olevskiy <arsidian@gmail.com>
2023-11-19 13:27:32 +0400

    calculator changes commit

--- plugin.php
+++ plugin.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Compound Interest Calculator by Calculator.iO
+Plugin Name: Compound Interest Calculator by www.calculator.io
 Plugin URI: https://www.calculator.io/compound-interest-calculator/
 Description: Compound interest calculator that uses the interest formula (A = P(1 + r/n)^nt) to help users understand the impacts of compound interest and money growth over time.
 Version: 1.0.0
@@ -16,7 +16,7 @@ if (!function_exists('add_shortcode')) return "No direct call for Compound Inter
 
 function display_ci_compound_interest_calculator(){
     $page = 'index.html';
-    return '<h2><a href="https://www.calculator.io/compound-interest-calculator/" target="_blank"><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48"></a> Compound Interest Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_compound_interest_calculator_iframe"></iframe></div>';
+    return '<h2><img src="' . esc_url(plugins_url('assets/images/icon-48.png', __FILE__ )) . '" width="48" height="48">Compound Interest Calculator</h2><div><iframe style="background:transparent; overflow: scroll" src="' . esc_url(plugins_url($page, __FILE__ )) . '" width="100%" frameBorder="0" allowtransparency="true" onload="this.style.height = this.contentWindow.document.documentElement.scrollHeight + \'px\';" id="ci_compound_interest_calculator_iframe"></iframe></div>';
 }
 
 add_shortcode( 'ci_compound_interest_calculator', 'display_ci_compound_interest_calculator' );
